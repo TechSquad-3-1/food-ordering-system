7e0e6571fa6f9c58d1479382ba7f19de2b2bba25
Hiruni Chamathka <148066088+chamahiru@users.noreply.github.com>
2025-03-28 03:16:46 -0300

    menu files rename in category

--- backend/menu-service/src/app.ts
+++ backend/menu-service/src/app.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import restaurantRoutes from './routes/restaurant.routes';
-import menuRoutes from './routes/menu.routes';
 import menuItemRoutes from './routes/menuItem.routes';
+import categoryRoutes from './routes/category.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/restaurants', restaurantRoutes);
-app.use('/api/menus', menuRoutes);
+app.use('/api/category', categoryRoutes);
 app.use('/api/menu-items', menuItemRoutes);
 
 export default app;
--- backend/menu-service/src/routes/menuItem.routes.ts
+++ backend/menu-service/src/routes/menuItem.routes.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 import { 
   createMenuItemHandler, 
-  getMenuItemsByMenuHandler, 
-  updateMenuItemHandler, // Import the new handler
-  deleteMenuItemHandler  // Import the new handler
+  getMenuItemsByCategoryHandler, // Updated handler name
+  updateMenuItemHandler, 
+  deleteMenuItemHandler,
+  getMenuItemsByRestaurantHandler 
 } from '../controllers/menuItem.controller';
 
 const router = express.Router();
 
 router.post('/', createMenuItemHandler); // Create a new menu item
-router.get('/:menuId', getMenuItemsByMenuHandler); // Get menu items by menu ID
+router.get('/:categoryId', getMenuItemsByCategoryHandler); // Updated route to categoryId
 router.put('/:menuItemId', updateMenuItemHandler); // Update a menu item by ID
 router.delete('/:menuItemId', deleteMenuItemHandler); // Delete a menu item by ID
+router.get('/restaurant/:restaurantId', getMenuItemsByRestaurantHandler); // Get menu items by restaurant ID
+
 
 export default router;
