4c77727845f30bb23ca28ebcf0d8bfb5ff734e0e
Kaweesha Nethmina <kaweesha.nj@gmail.com>
2025-03-30 23:17:22 +0530

    fix(ms): corrected cors

--- backend/menu-service/src/app.ts
+++ backend/menu-service/src/app.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import restaurantRoutes from './routes/restaurant.routes';
 import menuItemRoutes from './routes/menuItem.routes';
 import categoryRoutes from './routes/category.routes';
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/restaurants', restaurantRoutes);
